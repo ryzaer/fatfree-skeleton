6b8233d8ac622249d2b3485f1bb2000252530254
riza borneo <ryzaer@live.com>
2024-04-24 21:48:08 +0700

    Update html.php

--- controllers/minify/html.php
+++ controllers/minify/html.php
@@ -6,6 +6,6 @@ class html
     public static function emit(string $html, array $options = []) : string
     {
         $minifier = new core\TinyHtmlMinifier($options);
-        return $minifier->minify($html);
+        return preg_replace('/\>\s+\</','><',$minifier->minify($html));
     }
 }

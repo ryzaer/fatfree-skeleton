e797e1208c5c058258af2cac61efc3ae362dca6d
ryzaer <ryzaer@live.com>
2024-05-17 15:22:50 +0700

    fixed error models
    Signed-off-by: ryzaer <ryzaer@live.com>

--- controllers/models.php
+++ controllers/models.php
@@ -170,7 +170,7 @@ HTML;
                 "    \"It used to appear message from your browser that you are not able to access this web\",",
                 "    \"This is only example error messages that you can build something else\",",
                 "]);",
-                "\$f3->error_trace = array_values(array_filter(preg_split('/\n/',\$f3->get(\"ERROR.trace\"))));",
+                "\$f3->error_trace = array_values(array_filter(preg_split('/\\n/',\$f3->get(\"ERROR.trace\"))));",
 	            "\$f3->error_count = count(\$f3->error_trace);",
                 "http_response_code(\$f3->get(\"ERROR.code\"));",
                 "\$f3->view('app/templates/error.htm');"
